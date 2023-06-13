7891e224aed2bc6941e73f72bb981dd37409f8b7
yunkyung <133613667+Yunkyungii@users.noreply.github.com>
2023-06-13 18:17:47 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,5 +4,12 @@ $(function () {
         autoplay: true,
         autoplaySpeed: 4000,
         pauseOnHover: false,
-    })
+    });
+
+
+    $('.menu_list li').on('click', function (e) {
+        e.preventDefault();
+        let idx = $(this).index();
+        $('.menu_desc li').eq(idx).addClass('on').siblings().removeClass('on');
+    });
 })
