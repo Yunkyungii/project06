3223b3b2a610adfa6995bd0970442beee8375117
yunkyung <133613667+Yunkyungii@users.noreply.github.com>
2023-09-20 14:43:33 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -36,6 +36,23 @@ $(function () {
     });
 
 
+    $('.main_menu .inner').slick({
+        slidesToShow: 1,
+        arrows: false,
+        dots: true,
+        mobileFirst: true,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: 'unslick'
+            }
+        ]
+    });
+
+    $(window).on('resize', function () {
+        $('.main_menu .inner').slick('resize');
+    });
+
     $('.menu_list li').on('click', function (e) {
         e.preventDefault();
         let idx = $(this).index();
@@ -46,5 +63,24 @@ $(function () {
 
     $('.menu_list li').on('click', function () {
         $(this).addClass('on').siblings().removeClass('on');
-    })
+    });
+
+    // 반응형
+    $('.mo_btn').on('click', function () {
+        $(this).toggleClass('on');
+        $('.header .gnb').toggleClass('on');
+    });
+
+    $('.header .gnb>ul>li>a').on('click', function (e) {
+        if ($('.header .gnb').hasClass('on')) {
+            e.preventDefault();
+            $(this).next().stop().slideToggle();
+            $(this).parent().siblings().find('.sub').stop().slideUp();
+            // $(this).parent().siblings().find('.submenu').slideUp();
+        }
+
+    });
+
 })
+
+
